301dee322cc776e3bad411526b0f0f7dcb683a00
Andrew Fulton <afulton20@gmail.com>
2019-03-28 22:07:36 -0500

    Create Base Entities and Services

---
+++ Core/Entities/IAudibleEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Core.Data
+{
+    public interface IAudibleEntity
+    {
+        string CreationUser { get; set; }
+
+        DateTime CreationDateTime { get; set; }
+
+        string LastUpdateUser { get; set; }
+
+        DateTime? LastUpdateDateTime { get; set; }
+    }
+}
