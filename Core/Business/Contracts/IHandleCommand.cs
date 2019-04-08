0c876a7521242b8757bdf1a4807d4708f93d2108
Andrew Fulton <afulton20@gmail.com>
2019-04-08 12:45:21 -0500

    Move Data Contracts From DatabaseFactory to Core

---
+++ Core/Business/Contracts/IHandleCommand.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace DatabaseFactory.Data.Contracts
+{
+    public interface IHandleCommand<TCommand> where TCommand : ICommand
+    {
+        void Handle(TCommand command);
+    }
+}
---
+++ Core/Business/Contracts/IHandleQuery.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace DatabaseFactory.Data.Contracts
+{
+    public interface IHandleQuery<TQuery, TResult> where TQuery: IQuery<TResult>
+    {
+        TResult Handle(TQuery query);
+    }
+}
