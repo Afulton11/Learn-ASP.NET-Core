496cb3b0267643c869ec65adafe05fdbd78d208b
Andrew Fulton <afulton20@gmail.com>
2019-04-08 23:59:10 -0500

    Register Logger for CommandHandlers

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Business.CommandHandlers;
+using DatabaseFactory.Data.Contracts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,6 +11,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Web
 {
@@ -30,6 +33,9 @@ namespace Web
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            // Register logger for commandHandlers
+            services.AddScoped<ILogger, Logger<CommandHandler<ICommand>>>();
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
